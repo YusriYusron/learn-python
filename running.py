683108e49235418796f67e8a079e01de6c0aa200
Yusri Yusron <yusriyusron0530@gmail.com>
2019-11-16 10:35:41 +0700

    Make list and print variable use string

--- running.py
+++ running.py
@@ -6,3 +6,30 @@ print("Ulala ",a)
 name = "Ateng"
 print("Ulala %s" %name)
 
+# Print variable use String
+print("Namanya %s dan nomor %d" %(name,a))
+
+# Define List and print list
+myList = [1,2,3]
+print("Isi list-nya adalah %s "%myList)
+print("Isi list-nya adalah",myList)
+
+# Input
+num = input("Masukan sesuatu: ")
+print("Ini hasil output dari inputanmu",num)
+
+# Check Data Type
+x = 6
+print(type(x))
+x = 'Hello'
+print(type(x))
+
+# Branches
+if False:
+    print(1+1) # Output 2
+else:
+    print(1+2) # Output 3
+
+# Length of String not allowed for numerik
+string = "Si Ateng"
+print(len(string))
